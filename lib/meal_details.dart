69ec074c6d9051b4aac21263d06b09c69c195437
12kishansingh <ks888singh@gmail.com>
2024-06-24 18:52:09 +0530

    making meal_details scrollable

--- lib/meal_details.dart
+++ lib/meal_details.dart
@@ -7,16 +7,61 @@ class MealDetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          meal.title,
+        appBar: AppBar(
+          title: Text(
+            meal.title,
+          ),
         ),
-      ),
-      body: Image.network(
-        meal.imageUrl,
-        width: double.infinity,
-        fit: BoxFit.cover,
-      ),
-    );
+        body: SingleChildScrollView(
+          child: Column(// instead of clolumn use List View (its not centered) which gives auto scroll but singlechildscroolveiw gives
+            children: [
+              Image.network(
+                meal.imageUrl,
+                height: 300,
+                width: double.infinity,
+                fit: BoxFit.cover,
+              ),
+              const SizedBox(height: 14),
+              Text(
+                'Indgredients',
+                style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                      color: Theme.of(context).colorScheme.primary,
+                      fontWeight: FontWeight.bold,
+                    ),
+              ),
+              const SizedBox(height: 14),
+              for (final ingredient in meal.ingredients)
+                Text(
+                  ingredient,
+                  style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                        color: Theme.of(context).colorScheme.onSurface,
+                      ),
+                ),
+              const SizedBox(height: 24),
+              Text(
+                'Steps',
+                style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                      color: Theme.of(context).colorScheme.primary,
+                      fontWeight: FontWeight.bold,
+                    ),
+              ),
+              const SizedBox(height: 14),
+              for (final step in meal.steps)
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 12,
+                    vertical: 8,
+                  ),
+                  child: Text(
+                    step,
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                          color: Theme.of(context).colorScheme.onSurface,
+                        ),
+                  ),
+                ),
+            ],
+          ),
+        ));
   }
 }
